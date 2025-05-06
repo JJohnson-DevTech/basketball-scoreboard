2829ce54d4a856426298343e6d325cf735e67260
Juston Johnson <jjohnson.devtech@gmail.com>
2025-05-06 14:25:25 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,87 @@
+const homeScoreEl = document.getElementById("home-score");
+const guestScoreEl = document.getElementById("guest-score");
+
+let homeScore = 0;
+let guestScore = 0;
+
+function updateScore(team, points) {
+    if (team === 'home') {
+        homeScore += points;
+        homeScoreEl.textContent = homeScore;
+    } else if (team === 'guest') {
+        guestScore += points;
+        guestScoreEl.textContent = guestScore;
+    }
+}
+
+function resetHomeScore() {
+    homeScore = 0;
+    homeScoreEl.textContent = homeScore;
+}
+
+function resetGuestScore() {
+    guestScore = 0;
+    guestScoreEl.textContent = guestScore;
+}
+
+
+let minutes = 0;
+let seconds = 5;
+let currentQuarter = 4;
+const totalQuarters = 4;
+let timerInterval;
+
+const timerEl = document.getElementById("timer");
+const quarterEl = document.getElementById("quarter");
+const startBtn = document.getElementById("start-timer-btn");
+const stopBtn = document.getElementById("stop-timer-btn");
+
+function updateTimerEl() {
+    const min = minutes.toString().padStart(2, '0');
+    const sec = seconds.toString().padStart(2, '0');
+    timerEl.textContent = `${min}:${sec}`;
+}
+
+function startTimer() {
+    if (timerInterval || currentQuarter > totalQuarters) return;
+
+    timerInterval = setInterval(() => {
+        if (seconds === 0) {
+            if (minutes === 0) {
+                clearInterval(timerInterval);
+                timerInterval = null;
+                if (currentQuarter < totalQuarters) {
+                    currentQuarter++;
+                    minutes = 0;
+                    seconds = 5;
+                    quarterEl.textContent = `Quarter: ${currentQuarter}`;
+                    updateTimerEl();
+                    startTimer();
+                } else {
+                    quarterEl.textContent = `Final`;
+                }
+                return;
+            }
+            minutes--;
+            seconds = 59;
+        } else {
+            seconds--;
+        }
+        updateTimerEl();
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    minutes = 0;
+    seconds = 5;
+    currentQuarter = 4;
+    quarterEl.textContent = `Quarter: ${currentQuarter}`;
+    updateTimerEl();
+}
